b471a599625ff3884829bf83b948316e07eecd97
Bwlake <bwigal2@nastudents.org>
2024-05-29 08:25:16 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,64 @@
+# About
+
+StarLight is a web proxy that has features coming soon.
+
+since: 2024
+
+# Features
+
+- Tab Cloaking
+
+# Self Hosting
+
+> [!IMPORTANT]  
+> If you fork this repo, Please ⭐️ this one.
+
+### Console Method
+
+```
+git clone https://github.com/GoStarLight/StarLight
+```
+
+```
+cd StarLight
+```
+
+```
+npm i
+```
+
+```
+npm start
+```
+
+### Github Codespace
+
+> [!NOTE]  
+>  Github Codespace has a 120 minutes for free and 180 minutes for pay per month.
+
+```
+npm i
+```
+
+```
+npm start
+```
+
+> [!IMPORTANT]  
+> If you have issues, press the make public button.
+
+# Issues
+
+To make a Issue / Report
+
+- Press the Issue Tab
+- Tap Create Issue
+- Choose a template
+
+# Supporters
+
+Thank you to all of your supporters!
+
+<a href="https://github.com/GoStarLight/StarLight/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=GoStarLight/StarLight" />
+</a>
---
+++ public/assets/js/games.js
@@ -0,0 +1,53 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js", {
+      scope: __uv$config.prefix,
+    });
+  });
+}
+
+const imageContainer = document.getElementById("image-container");
+fetch("./assets/json/g.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((image) => {
+      const imageElement = document.createElement("a");
+      const imgContainer = document.createElement("div");
+      imgContainer.className = "image-container";
+      const img = document.createElement("img");
+      img.src = image.logo;
+      img.alt = image.title || "ERROR";
+      img.style.width = "150px";
+      img.style.height = "150px";
+      img.className = "classy";
+
+      const altText = document.createElement("div");
+      altText.textContent = img.alt;
+
+      imageElement.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (!image.alert) {
+          if (!image.redirect) {
+            let url = image.link;
+            localStorage.setItem(
+              "Iframe",
+              __uv$config.prefix + __uv$config.encodeUrl(url),
+            );
+            window.location.href = "go.html";
+          } else {
+            window.open(image.redirect);
+          }
+        } else {
+          alert(image.alert);
+        }
+      });
+
+      imgContainer.appendChild(img);
+
+      imageElement.appendChild(imgContainer);
+      imageContainer.appendChild(imageElement);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON data:", error);
+  });
